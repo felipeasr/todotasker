ef0cd4c7e020c3a3a5c7d3096b933c42b06d317b
felipe <54966505+felipeasr@users.noreply.github.com>
2022-04-23 15:05:51 -0300

    update

--- lib/ui/about/about_us.dart
+++ lib/ui/about/about_us.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../../utils/app_constant.dart';
 import '../../utils/app_util.dart';
@@ -40,11 +41,38 @@ class AboutUsPage extends StatelessWidget {
                     )
                   ],
                 ),
+              ),
+              Card(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: ListTile(
+                        subtitle: Text('Developed by @felipeasr'),
+                      ),
+                    ),
+                    ElevatedButton.icon(
+                      icon: Icon(Icons.web),
+                      onPressed: _launchURL(),
+                      label: Text('Git'),
+                    )
+                  ],
+                ),
               )
             ],
           ),
         ),
       ),
     );
+  }
+
+  _launchURL() {
+    const url = 'https://github.com/felipeasr/todotasker';
+    if (canLaunch(url) != null) {
+      launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 }
