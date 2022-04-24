c2029dfded075be19202addc67c5e87315486c27
felipe <54966505+felipeasr@users.noreply.github.com>
2022-04-24 18:02:58 -0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -13,8 +13,8 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
         theme: ThemeData(
-          accentColor: Colors.orange,
-          primaryColor: const Color(0xFFDE4435),
+          accentColor: Colors.blue,
+          primaryColor: Color.fromARGB(255, 0, 33, 55),
         ),
         home: BlocProvider(bloc: HomeBloc(), child: HomePage()));
   }
